089941694ca6037db9f44b6cb7d26dc5ab8d646a
Sergii Kantor <sergiy.kantor@gmail.com>
2022-10-18 03:21:45 +0300

    add taxiservicee.herokuapp.com to host

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "taxiservicee.herokuapp.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
