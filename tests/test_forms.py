07ea92af0d7b94cb41cf0e16d98267c37302160f
Sergii Kantor <sergiy.kantor@gmail.com>
2022-10-21 15:10:00 +0300

    add ome more test for form

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 from taxi.forms import DriverCreationForm, validate_license_number
 
@@ -13,6 +15,7 @@ class DriverFormTest(TestCase):
             "last_name": "test last name",
             "license_number": "QWE12345"
         }
+
         form = DriverCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, form_data)
@@ -22,3 +25,23 @@ class DriverFormTest(TestCase):
         self.assertTrue(validate_license_number(license_number))
 
 
+class PrivateDriverTest(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            "test", "Qwer123456"
+        )
+        self.client.force_login(self.user)
+
+    def test_create_drivers(self):
+        form_data = {
+            "username": "test",
+            "password1": "Qwer123456",
+            "password2": "Qwer123456",
+            "first_name": "test first name",
+            "last_name": "test last name",
+            "license_number": "QWE12345"
+        }
+
+        self.client.post(reverse("taxi:driver-create"), date=form_data)
+        new_user = get_user_model().objects.get(username=form_data["username"])
+        self.assertEqual(new_user.username, form_data["username"])
